b20cece29d8029906ac006a4e78e2418377f14b4
Herbrant <cdavide98carnemolla@gmail.com>
2023-07-20 18:45:26 +0200

    feat(godot): added websocket

--- godot/script/CharacterBody2D.gd
+++ godot/script/CharacterBody2D.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @export var speed = 400
 @export var rotation_speed = 1.5
 
+var server = TCPServer.new()
+var socket = WebSocketPeer.new()
+var peer: StreamPeerTCP
 var rotation_direction = 0
 
 func get_input():
@@ -13,3 +16,23 @@ func _physics_process(delta):
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
+
+func _ready():
+	server.listen(8000)
+	
+
+func _process(delta):
+	socket.poll()
+	peer = server.take_connection()
+	socket.accept_stream(peer)
+	
+	var state = socket.get_ready_state()
+	if state == socket.STATE_OPEN:
+		print("OPEN")
+	elif state == socket.STATE_CLOSING:
+		print("CLOSING")
+	elif state == socket.STATE_CLOSED:
+		print("CLOSED")
+	
+	
+	
--- godot/script/base.gd
+++ godot/script/base.gd
@@ -1,19 +1,56 @@
 extends Node2D
 
 
+var server = TCPServer.new()
+var socket = WebSocketPeer.new()
+var stream : StreamPeerTCP
+var PORT = 8080
+
+func load_map():
+	var file = FileAccess.open("res://config/matrix.txt", FileAccess.READ)
+	var line = ""
+	var map = []
+	
+	while (not file.eof_reached()):
+		line = file.get_line()
+		var tmp_arr = line.split(" ")
+		map.append(tmp_arr)
+	
+	return map
+
+func set_map():
+	var obstacle_scene = load("res://scene/Obstacle.tscn")
+	var screen_size = get_viewport_rect().size
+	print(screen_size)
+	
+	var m = load_map()
+	
+	for i in range(len(m)):
+		for j in range(len(m[i])):
+			if m[i][j] == "1":
+				var obj = obstacle_scene.instantiate()
+				obj.position.y = screen_size[0]/len(m) * i
+				obj.position.x = screen_size[1]/len(m[i]) * j
+				add_child(obj)
+
+
+
+func info(msg):
+	print(msg)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var obstacle_scene = load("res://scene/Obstacle.tscn")
-	var screen_size = get_viewport().get_visible_rect().size
-	
-	var test = obstacle_scene.instantiate()
-	test.position.x = 100
-	test.position.y = 150
-	add_child(test)
-	
-	
+	set_map()
+	server.listen(PORT)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var conn = server.is_connection_available()
+	if conn and !stream:
+		stream = server.take_connection()
+		stream.set_no_delay(true)
+		socket.accept_stream(stream)
+		
+		while socket.get_ready_state() != WebSocketPeer.STATE_OPEN:
+			socket.poll()
+		
+		socket.send_text("Test message from server")
