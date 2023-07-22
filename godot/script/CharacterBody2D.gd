f0d785c75fa1d002d7d6b753af24ec1cf32324ad
Herbrant <cdavide98carnemolla@gmail.com>
2023-07-23 01:54:18 +0200

    feat: updated godot module

--- godot/script/CharacterBody2D.gd
+++ godot/script/CharacterBody2D.gd
@@ -1,29 +1,16 @@
 extends CharacterBody2D
 
-@export var speed = 400
-@export var rotation_speed = 1.5
-
 const PORT = 8000
 
 var server = TCPServer.new()
 var socket = WebSocketPeer.new()
-
-var rotation_direction = 0
-
-func get_input():
-	rotation_direction = Input.get_axis("left", "right")
-	velocity = transform.x * Input.get_axis("down", "up") * speed
-
-func _physics_process(delta):
-	get_input()
-	rotation += rotation_direction * rotation_speed * delta
-	move_and_slide()
+var screen_size
 
 func _ready():
+	screen_size = get_viewport_rect().size
 	server.listen(PORT)
-	
 
-func _process(delta):
+func _physics_process(delta):
 	var err: Error
 	socket.accept_stream(server.take_connection())
 	socket.poll()
@@ -33,14 +20,18 @@ func _process(delta):
 	if state == WebSocketPeer.STATE_OPEN:
 		while socket.get_available_packet_count():
 			var packet = socket.get_packet()
+			var pos = packet.to_float32_array()
+			print("Packet received: ", pos)
 			
-			print("Packet: ",packet.get_string_from_utf8())
+			position.x = pos[0] * screen_size[0] * delta
+			position.y = pos[1] * screen_size[1] * delta
+			rotation = pos[2]
+			
+			velocity = Vector2.ZERO
+			move_and_slide()
+			
 	
-	
-	# Position updates
-	var v: Vector2
-	v.x = 1
-	v.y = 1
-	global_translate(v)
-	move_and_slide()
-	rotate(10)
+
+func _process(delta):
+	pass
+
